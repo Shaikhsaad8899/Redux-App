6dd956a7543c7fdcd07b26729d5747642770502e
Saad Shaikh <saadsk9288@gmail.com>
2023-01-10 00:36:18 +0530

    add

--- src/features/homepage/formReducer.js
+++ src/features/homepage/formReducer.js
@@ -4,10 +4,10 @@ const initialState = {
 export const formReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_USER":
-      // console.log({
-      //     ...state,
-      //     users: [...state.users, action.payload],
-      //   })
+      console.log({
+          ...state,
+          users: [...state.users, action.payload],
+        })
       return {
         ...state,
         users: [...state.users, action.payload],
